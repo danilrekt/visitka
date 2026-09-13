b73ccc9da32d85cd4845f6ad57ef1d205d3abcd0
danilrekt <danilrekt1234@gmail.com>
2026-09-13 16:45:56 +0500

    add index_1.html

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { copyFileSync, existsSync } from 'fs'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'copy-index-1',
+      closeBundle() {
+        if (existsSync('index_1.html')) {
+          copyFileSync('index_1.html', 'dist/index_1.html')
+        }
+      },
+    },
+  ],
 })
