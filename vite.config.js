585834af3ea0d9d6dae457673d1ab27c60e2ea7d
danilrekt <danilrekt1234@gmail.com>
2026-09-20 03:22:21 +0500

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,8 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import { copyFileSync, existsSync } from 'fs'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [
-    react(),
-    {
-      name: 'copy-static',
-      closeBundle() {
-        const files = ['index_1.html', 'IMG_0033.jpeg']
-        files.forEach((f) => {
-          if (existsSync(f)) {
-            copyFileSync(f, `dist/${f}`)
-          }
-        })
-      },
-    },
-  ],
+  plugins: [react()],
 })
